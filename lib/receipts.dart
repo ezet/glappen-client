190e223e0102b96fccbf26387b88c7900d8f5528
Lars-Kristian Dahl <lars.dahl@gmail.com>
2019-08-17 22:51:37 +0200

    ui updates

--- lib/receipts.dart
+++ lib/receipts.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:garderobel_api/garderobel_client.dart';
 import 'package:garderobel_api/models/reservation.dart';
 import 'package:provider/provider.dart';
@@ -29,13 +30,49 @@ class ReceiptsList extends StatelessWidget {
     user = Provider.of<FirebaseUser>(context);
     // TODO: implement build
     final list = Provider.of<Iterable<Reservation>>(context)?.toList() ?? [];
-    return ListView.builder(
-      itemBuilder: (context, i) => _buildListItem(context, list[i]),
-      itemCount: list?.length ?? 0,
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 100, horizontal: 0),
+        child: Swiper(
+          itemBuilder: (context, i) => _buildListItem(context, list[i]),
+          itemCount: list?.length ?? 0,
+          loop: false,
+          // controller: SwiperController(),
+        ),
+      ),
     );
+
+    // return ListView.builder(
+    //   itemBuilder: (context, i) => _buildListItem(context, list[i]),
+    //   itemCount: list?.length ?? 0,
+    // );
   }
 
   Widget _buildListItem(BuildContext context, Reservation item) {
+    return Card(
+      // color: Colors.grey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+              child: Container(
+            decoration: BoxDecoration(
+              color: Colors.orangeAccent,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(5))),
+            child: Text(
+              "Test",
+              style: TextStyle(fontSize: 20),
+            ),
+          )),
+          Expanded(child: Container(),
+          flex: 3,)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListItem2(BuildContext context, Reservation item) {
     return Card(
       child: ListTile(
         onTap: () async {
