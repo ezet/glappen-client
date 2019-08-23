38d21e4250c8d5421cb49bad91092e3491f1c432
Lars-Kristian Dahl <lars.dahl@gmail.com>
2019-08-23 23:13:12 +0200

    Misc

--- lib/receipts.dart
+++ lib/receipts.dart
@@ -16,13 +16,93 @@ class Receipts extends StatelessWidget {
     final api = locator.get<GarderobelClient>();
     final user = Provider.of<FirebaseUser>(context);
     final reservations = api.findReservationsForUser(user.uid);
-    return StreamProvider.value(value: reservations, child: ReceiptsList());
+    return StreamProvider.value(value: reservations, child: RecieptHandler());
+  }
+}
+
+class RecieptHandler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final list = Provider.of<Iterable<Reservation>>(context)?.toList() ?? [];
+    if (list.length > 0 && list[0].paymentState == PaymentState.INITIAL) {
+      return Container();
+    } else {
+      return ReceiptsList();
+    }
   }
 }
 
 class ReceiptsList extends StatelessWidget {
   GarderobelClient api;
   FirebaseUser user;
+
+  Widget _buildListItem(BuildContext context, Reservation item) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 50, left: 5, right: 5, top: 40),
+      child: Card(
+        // color: Colors.grey,
+        elevation: 12,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+                flex: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.orangeAccent,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(5))),
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            item.venueName,
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: Text(item.hangerName,
+                              style: TextStyle(
+                                  fontSize: 60, fontWeight: FontWeight.w900)),
+                        ),
+                      ),
+                      Expanded(
+                        child: Center(
+                            child: Text(item.wardrobeName ?? "Wardrobe")),
+                        flex: 1,
+                      )
+                    ],
+                  ),
+                )),
+            Expanded(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text(item.paymentState.toString()),
+                      RaisedButton(
+                        onPressed: () {},
+                        child: Text('HENT'),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              flex: 3,
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,71 +120,8 @@ class ReceiptsList extends StatelessWidget {
           viewportFraction: 0.8,
           scale: 0.93,
           // pagination: SwiperPagination(
-              // builder: DotSwiperPaginationBuilder(color: Colors.grey, size: 5),
-              // alignment: Alignment.topCenter),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, Reservation item) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 50, left: 5, right: 5, top: 40),
-      child: Card(
-        // color: Colors.grey,
-        elevation: 12,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-                flex: 2,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.orangeAccent,
-                      borderRadius: BorderRadius.vertical(top: Radius.circular(5))),
-                  child: Column(
-                    children: <Widget>[
-                      Expanded(
-                        child: Center(
-                          child: Text(
-                            item.venueName,
-                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Center(
-                          child: Text(item.hangerName,
-                              style: TextStyle(fontSize: 60, fontWeight: FontWeight.w900)),
-                        ),
-                      ),
-                      Expanded(
-                        child: Center(child: Text(item.wardrobeName ?? "Wardrobe")),
-                        flex: 1,
-                      )
-                    ],
-                  ),
-                )),
-            Expanded(
-              child: Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Text(item.paymentState.toString()),
-                      RaisedButton(
-                        onPressed: () {},
-                        child: Text('HENT'),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              flex: 3,
-            )
-          ],
+          // builder: DotSwiperPaginationBuilder(color: Colors.grey, size: 5),
+          // alignment: Alignment.topCenter),
         ),
       ),
     );
